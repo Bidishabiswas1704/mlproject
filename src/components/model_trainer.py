75da2ca78d094de4eb779623949c6d2dbea51cbd
Bidisha Biswas <bidisha1704@gmail.com>
2023-10-28 21:35:21 +0530

    Your commit message

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,17 +37,60 @@ class ModelTrainer:
                 test_array[:,-1] 
             )
             models= {
-                "Random Forest": RandomForestRegressor()
-                "Decision Tree": DecisionTreeRegressor()
-                "K-Neighbour Classifier": KNeighborsRegressor()
-                "Gradient Boosting": GradientBoostingRegressor()
-                "AdaBoost Classifier": AdaBoostRegressor()
-                "XGBClassifier": XGBRegessor()
-                "Linear Regression": LinearRegression()
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "K-Neighbour Classifier": KNeighborsRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "AdaBoost Classifier": AdaBoostRegressor(),
+                "XGBClassifier": XGBRegessor(),
+                "Linear Regression": LinearRegression(),
                 "CatBoosting Classifier": CatBoostRegressor()
             }
-
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params= {
+                "Random Forest":{
+                    # 'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "n_estimators": [8,16,32,64,128,256],
+                    #"max_features": ["auto", "sqrt", "log2","none"],
+                },
+                "Decision Tree":{
+                    # 'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "max_depth": [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 3]
+                    #"max_features": ["auto", "sqrt", "log2","none"],
+                    # 'splitter' : ['best', 'randon']
+                },
+                "AdaBoost Classifier":{
+                    "n_estimators" : [8,16,32,64,128,256],
+                    'learning_rate': [.1,.01,.05,.001],
+                    # 'loss' : ['linear', 'square', 'exponential']
+                },
+                "K-Neighbour Classifier":{
+                    "n_neighbors": [5,7,9,11],
+                    # 'weights': ['uniform', 'distance'],
+                    # 'algorithm': ['ball_tree', 'kd_tree', 'brute']
+                },
+                "Gradient Boosting":{
+                    # 'loss' : ['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate': [.1,.01,.05,.001],
+                    # 'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "n_estimators": [8,16,32,64,128,256],
+                    #"max_features": ["auto", "sqrt", "log2","none"]
+                },
+                "XGBClassifier":{
+                    "n_estimators": [8,16,32,64,128,256],
+                    "learning_rate": [.1,.01,.05,.001]
+                },
+                "Linear Regression":{
+                    # 'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "fit_intercept": [True, False]
+                    #"max_features": ["auto", "sqrt", "log2","none"],
+                },
+                "CatBoosting Classifier":{
+                   'learning_rate': [.1,.01,.05,.001],
+                    'depth' : [6,8,10],
+                    'iterations' : [30, 50, 100]
+                }
+            }
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             ##To get best model score from dict 
             best_model_score = max(sorted(model_report.values()))
